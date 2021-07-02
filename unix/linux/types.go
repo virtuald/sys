1c29a18137dd291042b054cb830b450464fe82c1
Dustin Spicuzza <dustin.spicuzza@gmail.com>
2021-07-02 11:45:39 -0400

    Use shmid64_ds and ipc64_perm on Linux

--- unix/linux/types.go
+++ unix/linux/types.go
@@ -34,14 +34,12 @@ package unix
 #include <sys/epoll.h>
 #include <sys/inotify.h>
 #include <sys/ioctl.h>
-#include <sys/ipc.h>
 #include <sys/mman.h>
 #include <sys/mount.h>
 #include <sys/param.h>
 #include <sys/ptrace.h>
 #include <sys/resource.h>
 #include <sys/select.h>
-#include <sys/shm.h>
 #include <sys/signal.h>
 #include <sys/signalfd.h>
 #include <sys/statfs.h>
@@ -106,6 +104,7 @@ struct termios2 {
 #include <linux/if_packet.h>
 #include <linux/if_pppox.h>
 #include <linux/if_xdp.h>
+#include <linux/ipc.h>
 #include <linux/keyctl.h>
 #include <linux/loop.h>
 #include <linux/lwtunnel.h>
@@ -126,6 +125,7 @@ struct termios2 {
 #include <linux/random.h>
 #include <linux/rtc.h>
 #include <linux/rtnetlink.h>
+#include <linux/shm.h>
 #include <linux/socket.h>
 #include <linux/stat.h>
 #include <linux/taskstats.h>
@@ -3801,8 +3801,8 @@ const (
 
 // shm
 
-type SysvIpcPerm C.struct_ipc_perm
-type SysvShmDesc C.struct_shmid_ds
+type SysvIpcPerm C.struct_ipc64_perm
+type SysvShmDesc C.struct_shmid64_ds
 
 const (
 	IPC_CREAT   = C.IPC_CREAT
